ba06ab28662c49b0dbd7e82dce9e8b0d3fcab3bd
Dr Nic Williams <drnicwilliams@gmail.com>
2020-03-11 14:53:06 +1000

    separate deploy for pr

--- .buildkite/scripts/fake-test.sh
+++ .buildkite/scripts/fake-test.sh
@@ -7,3 +7,15 @@ sleep 1
 
 echo "--- :linux: Environment"
 env | sort
+
+# If a pull request:
+# BUILDKITE_BRANCH=show-env
+# BUILDKITE_PULL_REQUEST=1
+# BUILDKITE_PULL_REQUEST_BASE_BRANCH=master
+# BUILDKITE_PULL_REQUEST_REPO=git://github.com/drnic/buildkite-community-pr-flow-example.git
+
+# If a branch:
+# BUILDKITE_BRANCH=show-env
+# BUILDKITE_PULL_REQUEST=false
+# BUILDKITE_PULL_REQUEST_BASE_BRANCH=
+# BUILDKITE_PULL_REQUEST_REPO=
