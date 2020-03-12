7a6a64f4e9b1c0a33ae0a5cd2e0a8dcfde6a5895
Dr Nic Williams <drnicwilliams@gmail.com>
2020-03-12 20:00:58 +1000

    set $SUBDOMAIN for url

--- .buildkite/scripts/fake-deploy.sh
+++ .buildkite/scripts/fake-deploy.sh
@@ -23,7 +23,7 @@ echo "--- :cloudfoundry: Annotating"
 sleep 1
 
 url=https://myapp.com
-[[ -n "${BUILDKITE_PULL_REQUEST:-}" ]] && {
-  url=https://pr-${BUILDKITE_PULL_REQUEST}.myapp.com
+[[ -n "${SUBDOMAIN:-}" ]] && {
+  url=https://${SUBDOMAIN}.myapp.com
 }
 echo "--- :cloudfoundry: Deployed to $(inline_link $url)"
