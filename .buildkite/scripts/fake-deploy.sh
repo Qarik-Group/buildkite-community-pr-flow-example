4928c22b2050f1ded21687cd6d1043b410de58ed
Dr Nic Williams <drnicwilliams@gmail.com>
2020-03-12 17:23:04 +1000

    Showing deployed URL

--- .buildkite/scripts/fake-deploy.sh
+++ .buildkite/scripts/fake-deploy.sh
@@ -2,6 +2,16 @@
 
 set -euo pipefail
 
+function inline_link {
+  LINK="url='"$1"'"
+
+  if [ $# -gt 1 ]; then
+    LINK="$LINK;content='"$2"'"
+  fi
+
+  printf '\033]1339;'$LINK'\a\n'
+}
+
 echo "--- :cloudfoundry: Preparing"
 sleep 1
 echo "--- :cloudfoundry: Configuring"
@@ -10,3 +20,6 @@ echo "--- :cloudfoundry: Deploying"
 sleep 1
 echo "--- :cloudfoundry: Annotating"
 sleep 1
+
+url=https://myapp.com
+echo "--- :cloudfoundry: Deployed to $(inline_link $url)"
