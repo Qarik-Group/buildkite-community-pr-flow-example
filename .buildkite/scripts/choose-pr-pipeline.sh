05aabe77def5b26e074d2d577bb588a284653764
Dr Nic Williams <drnicwilliams@gmail.com>
2020-03-13 16:28:22 +1000

    allow $PR_WHITELIST of github.com/user-or-org for friendly vs guest pr pipeline

--- .buildkite/scripts/choose-pr-pipeline.sh
+++ .buildkite/scripts/choose-pr-pipeline.sh
@@ -5,8 +5,17 @@ set -euo pipefail
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 cd "$ROOT"
 
-if [[ $BUILDKITE_PULL_REQUEST_REPO =~ github.com/starkandwayne ]]; then
-  cat .buildkite/pipeline.pr-friendly.yml
-else
+: "${PR_WHITELIST:=}"
+found=no
+for whitelist_org in $PR_WHITELIST; do
+  if [[ $BUILDKITE_PULL_REQUEST_REPO =~ $whitelist_org ]]; then
+    echo "# friendly"
+    cat .buildkite/pipeline.pr-friendly.yml
+    found=yes
+  fi
+done
+
+[[ "${found}" == "no" ]] && {
+  echo "# guest"
   cat .buildkite/pipeline.pr-guest.yml
-fi
+}
